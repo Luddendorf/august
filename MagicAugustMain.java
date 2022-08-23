b1d667e886f6438cc802779c335cb2ac49523282
dmytro.filipichev@ihsmarkit.com <dmytro.filipichev@ihsmarkit.com>
2022-08-23 21:42:14 +0300

    Checkbox handler added

--- MagicAugustMain.java
+++ MagicAugustMain.java
@@ -1,6 +1,15 @@
 public class MagicAugustMain {
 
+   public boolean hasUserAgreedToTeams;
+
    public static void main(String[] args){
 	  System.out.println("Hello August");
 	}
+
+
+    public boolean getCheckboxStatus(boolean newStatus) {
+        this.hasUserAgreedToTeams = !this.hasUserAgreedToTeams;
+
+        return this.hasUserAgreedToTeams
+    }
 }
