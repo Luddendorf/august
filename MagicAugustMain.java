05ab616c4c0ae3f9ea53e8c6c2ddcea9e33b0b37
dmytro.filipichev@ihsmarkit.com <dmytro.filipichev@ihsmarkit.com>
2022-08-23 21:49:31 +0300

    Added name getter method

--- MagicAugustMain.java
+++ MagicAugustMain.java
@@ -3,4 +3,8 @@ public class MagicAugustMain {
    public static void main(String[] args){
 	  System.out.println("Hello August");
 	}
+
+    public String getMyName(String name) {
+        return name;
+    }
 }
